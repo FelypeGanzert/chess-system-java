2915b961f813ccccaddf8bcfa1c82c47b19cb7be
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2020-06-22 14:57:41 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -54,7 +54,23 @@ public class UI {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print(pieces.length - i + " ");
 			for (int j = 0; j < pieces[i].length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
+			}
+			System.out.println();
+		}
+
+		System.out.print("  ");
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print(alphabet[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print(pieces.length - i + " ");
+			for (int j = 0; j < pieces[i].length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
@@ -66,9 +82,12 @@ public class UI {
 		System.out.println();
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if(background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
