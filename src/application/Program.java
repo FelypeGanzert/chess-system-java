e5ba907630a8b643f87d20a1a12c790b76554e43
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2020-06-28 01:45:14 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,13 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			} catch(ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
@@ -50,7 +57,5 @@ public class Program {
 		
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
-		
-		
 	}
 }
